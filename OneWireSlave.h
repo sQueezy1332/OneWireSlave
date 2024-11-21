eaad2feb29494d57641dc25779f437e2b069d9a7
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2024-11-21 11:32:39 +0300

    Update OneWireSlave.h

--- OneWireSlave.h
+++ OneWireSlave.h
@@ -7,13 +7,24 @@
 
 #pragma once
 #ifdef __cplusplus
+
 #include <stdint.h>
+
 #if defined(__AVR__)
 #include <util/crc16.h>
 #endif
 
 #include <Arduino.h>       // for delayMicroseconds, digitalPinToBitMask, etc
-#define pMode(pin, mode)    pinMode(pin, mode)
+#include <GyverIO.h>
+#define pInit(pin, mode)    gio::init(pin, mode)
+#define DIRECT_READ(pin)	gio::read(pin)
+#if defined(__AVR__)
+#define DIRECT_WRITE_HIGH(pin)	gio::mode(pin, INPUT)
+#define DIRECT_WRITE_LOW(pin) gio::mode(pin, OUTPUT)
+#else
+#define DIRECT_WRITE_HIGH(pin)	gio::high(pin)	//OPEN_DRAIN
+#define DIRECT_WRITE_LOW(pin) gio::low(pin)
+#endif
 #define uS micros()
 #define mS millis()
 
@@ -35,20 +46,20 @@
 // or a small but slow algorithm.
 
 // Board-specific macros for direct GPIO
-#include "util/OneWire_direct_regtype.h"
+//#include "util/OneWire_direct_regtype.h"
 //#define microsecondsToClockCycles(a) ( (a) * clockCyclesPerMicrosecond() )
 //#define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
 #if defined(__AVR__)
 #define divider 18uL //25 w/o read 100000 is 10088-10092 //30 with READ 10048-10056 (~10056)   
 #endif 
-#if defined(ARDUINO_ARCH_ESP32)
+#if defined(ARDUINO_ARCH_ESP32) || defined(ESP8266)
 #define divider 27uL //11 with out read 10000 is 10075 // 27 with read 10000 is 10077 
 #endif 
 //These are the major change from original, we now wait quite a bit longer for some things
 #define TIMESLOT_WAIT_RETRY_COUNT microsecondsToClockCycles(120) / divider //295000 Maximum with interrupts
 //This TIMESLOT_WAIT_READ_RETRY_COUNT is new, and only used when waiting for the line to go low on a read
 //It was derived from knowing that the Arduino based master may go up to 130 micros more than our wait after reset
-#define TIMESLOT_WAIT_RECOVERY_RETRY_COUNT microsecondsToClockCycles(1000) / divider
+#define TIMESLOT_WAIT_RECOVERY_RETRY_COUNT microsecondsToClockCycles(2000) / divider
 
 #define ONEWIRESLAVE_CRC 1
 // Select the table-lookup method of computing the 8-bit CRC
@@ -58,44 +69,39 @@
 #define CMD 0xAA
 typedef uint8_t byte;
 
-__attribute__((weak))  extern const byte pin_onewireslave;
+__attribute__((weak)) extern const byte pin_onewire;
 
 class OneWireSlave {
 private:
 	byte* rom = nullptr;
 protected:
-	IO_REG_TYPE bitmask;
-	volatile IO_REG_TYPE* baseReg;
 	void waitTimeSlot();
 public:
-	OneWireSlave() { begin(pin_onewireslave); }
-	OneWireSlave(byte pin) { begin(pin); };
+	OneWireSlave() { begin(pin_onewire); }
 	void begin(byte pin);
 	void init(byte buf[8], bool crc_set = false);
 	bool waitForRequest(uint16_t timeout_ms = 1000, bool ignore_errors = false);
 	bool recvAndProcessCmd();
-	bool waitReset(uint16_t timeout_ms);
+	bool waitReset(uint16_t timeout_ms = 1000);
 	bool presence();
 	byte recvData(byte buf[], byte lenght = 8);
 	byte recvByte();
 	bool recvBit();
-	byte sendData(const byte buf[], byte lenght = 8);
+	byte sendData(const byte* const& buf, byte lenght = 8);
 	void sendByte(const byte v);
 	void sendBit(const bool bit);
 	bool search();
 	typedef enum {
 		ONEWIRE_NO_ERROR = 0,
-		ONEWIRE_READ_TIMESLOT_TIMEOUT = 1,
-		ONEWIRE_WRITE_TIMESLOT_TIMEOUT = 2,
-		ONEWIRE_WAIT_RESET_TIMEOUT = 3,
-		ONEWIRE_VERY_LONG_RESET = 4,
-		ONEWIRE_VERY_SHORT_RESET = 5,
-		ONEWIRE_PRESENCE_LOW_ON_LINE = 6,
-		ONEWIRE_READ_TIMESLOT_TIMEOUT_LOW = 7,
-		ONEWIRE_READ_TIMESLOT_TIMEOUT_HIGH = 8
+		ONEWIRE_READ_TIMESLOT_TIMEOUT_LOW,
+		ONEWIRE_READ_TIMESLOT_TIMEOUT_HIGH,
+		ONEWIRE_WAIT_RESET_TIMEOUT,
+		ONEWIRE_VERY_LONG_RESET,
+		ONEWIRE_VERY_SHORT_RESET,
+		ONEWIRE_PRESENCE_LOW_ON_LINE
 	} err_t;
 #if ONEWIRESLAVE_CRC
-	byte crc8(byte addr[], byte len);
+	byte crc8(const byte addr[], byte len);
 #endif
 	err_t error = ONEWIRE_NO_ERROR;
 };
@@ -103,13 +109,11 @@ public:
 class OneWireSniffer : public OneWireSlave {
 public:
 	OneWireSniffer() : OneWireSlave() {};
-	bool waitForRequest(byte buf[], byte& cmd, uint16_t timeout_ms = 1000, bool ignore_errors = true);
+	bool waitForRequest(byte buf[8], byte& cmd, uint16_t timeout_ms = 1000, bool ignore_errors = true);
 	bool presenceDetection();
-	bool recvAndProcessCmd(byte buf[], byte& cmd);
-	bool searchAndReceive(byte buf[]);
+	bool recvAndProcessCmd(byte* const& buf, byte& cmd);
+	bool searchAndReceive(byte* const& buf);
 };
 
 //static OneWireSlave* static_OWS_instance;
 #endif
-
-
